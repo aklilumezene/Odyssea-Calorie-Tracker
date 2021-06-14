72e5f99d775f25c5d502d75075725d30f00fe28d
aklilu <aklilumezene@gmail.com>
2021-06-14 23:56:57 +0300

    MealItem ~ use array of objects

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import MealItem from "./components/MealItem"
 
 function App() {
+  const meals = [
+    {
+      timestamp: new Date(2021, 4, 12, 12, 39, 0, 0),
+      description: "burger 100g",
+      meal: "Lunch",
+      calorie: 237.7,
+    },
+    {
+      timestamp: new Date(2021, 4, 11, 9, 32, 0, 0),
+      description: "150g chicken soup",
+      meal: "Breakfast",
+      calorie: 49.8,
+    },
+    {
+      timestamp: new Date(2021, 4, 11, 21, 40, 0, 0),
+      description: "200g Green salad",
+      meal: "Dinner",
+      calorie: 46.9,
+    },
+    {
+      timestamp: new Date(2021, 4, 12, 13, 30, 0, 0),
+      description: "100g filet",
+      meal: "Lunch",
+      calorie: 272.6,
+    },
+    {
+      timestamp: new Date(2021, 4, 12, 8, 23, 0, 0),
+      description: "100g Oats",
+      meal: "Breakfast",
+      calorie: 386.8,
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello React!</h1>
+      <MealItem calorie={meals[0].calorie} meal={meals[0].meal} description={meals[0].description} date={meals[0].timestamp} />
+      <MealItem calorie={meals[1].calorie} meal={meals[1].meal} description={meals[1].description} date={meals[1].timestamp} />
+      <MealItem calorie={meals[2].calorie} meal={meals[2].meal} description={meals[2].description} date={meals[2].timestamp} />
+      <MealItem calorie={meals[3].calorie} meal={meals[3].meal} description={meals[3].description} date={meals[3].timestamp} />
+      <MealItem calorie={meals[4].calorie} meal={meals[4].meal} description={meals[4].description} date={meals[4].timestamp} />
     </div>
+
   );
 }
 
---
+++ src/components/MealItem.js
@@ -0,0 +1,14 @@
+import "./MetricItem.css"
+
+const MealItem = (props) => {
+    return <div className="meal-item">
+    <div className="meal-item__date">{props.date.toISOString()}</div>
+    <div className="meal-item__meal">
+        <div>{props.description}</div>
+        <h3>{props.meal}</h3>
+    </div>
+    <div className="meal-item__calorie">{props.calorie}</div>
+</div>
+}
+
+export default MealItem
