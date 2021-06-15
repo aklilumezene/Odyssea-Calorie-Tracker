f6cbbf7ee5bd22b8877fc284211807f59041c064
aklilu <aklilumezene@gmail.com>
2021-06-16 02:47:01 +0300

    Add Card component

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,18 +1,19 @@
 import "./MealItem.css";
 import MealDate from "./MealDate";
+import Card from "./Card"
 
 const MealItem = (props) => {
   const dispCalorie = props.calorie === 0 ? "" : props.calorie;
 
   return (
-    <div className="meal-item">
+    <Card className="meal-item">
       <div className="meal-item__meal">
         <MealDate date={props.date} />
         <div>{props.description}</div>
         <h3>{props.meal}</h3>
       </div>
       <div className="meal-item__calorie">{dispCalorie}</div>
-    </div>
+    </Card>
   );
 };
 
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,15 +1,16 @@
 import "./Meals.css"
 import MealItem from "./MealItem"
+import Card from "./Card"
 
 const Meals = (props) => {
     return(
- <div className = "meals">
+ <Card className = "meals">
      <MealItem calorie={props.data[0].calorie} meal={props.data[0].meal} description={props.data[0].description} date={props.data[0].timestamp} />
       <MealItem calorie={props.data[1].calorie} meal={props.data[1].meal} description={props.data[1].description} date={props.data[1].timestamp} />
       <MealItem calorie={props.data[2].calorie} meal={props.data[2].meal} description={props.data[2].description} date={props.data[2].timestamp} />
       <MealItem calorie={props.data[3].calorie} meal={props.data[3].meal} description={props.data[3].description} date={props.data[3].timestamp} />
       <MealItem calorie={props.data[4].calorie} meal={props.data[4].meal} description={props.data[4].description} date={props.data[4].timestamp} />
- </div>
+ </Card>
     );
 }
 
