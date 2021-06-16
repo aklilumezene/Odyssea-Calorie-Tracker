c3287f477146ac6433811117c792c5d41b833536
aklilu <aklilumezene@gmail.com>
2021-06-16 17:50:15 +0300

    AddMeal: read user input 1 file changed

--- src/components/MealForm.js
+++ src/components/MealForm.js
@@ -1,24 +1,58 @@
+import userEvent from "@testing-library/user-event";
+import { useState } from "react";
 import "./MealForm.css";
 
 const MealForm = (props) => {
+
+     const [userDate, setUserDate] = useState("")
+
+     const dateChangedHandler = (event) => {
+         let newValue = event.target.value
+         setUserDate(newValue)
+         console.log(`someone typed ${userDate}`)
+     }
+
+     const [userMeal, setUserMeal] = useState("")
+     
+     const mealChangedHandler = (event) => {
+         let newValue = event.target.value
+         setUserMeal(newValue)
+         console.log(`someone typed ${userMeal}`)
+     }
+
+     const [userDescription, setUserDescription] = useState("")
+
+     const descriptionChangedHandler = (event) => {
+        let newValue = event.target.value
+        setUserDescription(newValue)
+        console.log(`someone typed ${userDescription}`)
+    }
+
+     const [userCalorie, setUserCalorie] = useState("")
+
+     const calorieChangedHandler = (event) => {
+         let newValue = event.target.value
+         setUserCalorie(newValue)
+         console.log(`someone typed ${userCalorie}`)
+     }
     return (
         <form>
             <div className="add-meal__elements">
                 <div className="add-meal__element">
                     <label>Date</label>
-                    <input type="datetime-local" />
+                    <input type="datetime-local" onChange={dateChangedHandler}/>
                 </div>
                 <div className="add-meal__element">
                     <label>Meal</label>
-                    <input type="text" />
+                    <input type="text" onChange={mealChangedHandler} />
                 </div>
                 <div className="add-metric__element">
                     <label>Description</label>
-                    <input type="text" />
+                    <input type="text" onChange={descriptionChangedHandler} />
                 </div>
                 <div className="add-meal__element">
                     <label>Calorie</label>
-                    <input type="number" min="0" step="1" />
+                    <input type="number" min="0" step="1" onChange={calorieChangedHandler} />
                 </div>
 
             </div>
