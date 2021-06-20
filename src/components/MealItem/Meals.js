7d635a3e2d76bd2822ef7e2af3eddfdce0e92c4c
aklilu <aklilumezene@gmail.com>
2021-06-20 21:37:31 +0300

    Properly render array (using map)

--- src/components/MealItem/Meals.js
+++ src/components/MealItem/Meals.js
@@ -1,17 +1,22 @@
-import "./Meals.css"
-import MealItem from "./MealItem"
-import Card from "../UI/Card"
+import "./Meals.css";
+import MealItem from "./MealItem";
+import Card from "../UI/Card";
 
 const Meals = (props) => {
-    return(
- <Card className = "meals">
-     <MealItem calorie={props.data[0].calorie} meal={props.data[0].meal} description={props.data[0].description} date={props.data[0].timestamp} />
-      <MealItem calorie={props.data[1].calorie} meal={props.data[1].meal} description={props.data[1].description} date={props.data[1].timestamp} />
-      <MealItem calorie={props.data[2].calorie} meal={props.data[2].meal} description={props.data[2].description} date={props.data[2].timestamp} />
-      <MealItem calorie={props.data[3].calorie} meal={props.data[3].meal} description={props.data[3].description} date={props.data[3].timestamp} />
-      <MealItem calorie={props.data[4].calorie} meal={props.data[4].meal} description={props.data[4].description} date={props.data[4].timestamp} />
- </Card>
-    );
-}
+  return (
+    <Card className="meals">
+      {props.data.map((item, index) => {
+        return (
+          <MealItem
+            date={props.data[index].timestamp}
+            meal={props.data[index].meal}
+            description={props.data[index].description}
+            calorie={props.data[index].calorie}
+          />
+        );
+      })}
+    </Card>
+  );
+};
 
-export default Meals
+export default Meals;
